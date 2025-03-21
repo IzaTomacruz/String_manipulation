f149d5159b1ac95704971f489373976aae4575d7
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-22 07:46:32 +0800

    Created a program for reverse_casing.py

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,10 +1,9 @@
-#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
-#Example:
-#Input: jUAn DEla CrUZ
-#Output: JuaN deLA cRuz
-
 # Ask for a full name
+print("What is you name?")
+name = input("Name: ")
 
 # Convert reverse the incorrect casing
+reverse_case = name.swapcase()
 
 # Print the result
+print("\nOutput:", reverse_case)
