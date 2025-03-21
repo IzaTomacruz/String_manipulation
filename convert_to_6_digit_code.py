bd0572b5a1576baf78930dfbdc07aa827542fabc
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-22 07:12:37 +0800

    Created program for convert_to_6_digit_code.py

--- convert_to_6_digit_code.py
+++ convert_to_6_digit_code.py
@@ -1,10 +1,9 @@
-#Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
-#Example:
-#Input: 143
-#Output: 000143
-
 # Ask for a number
+print("input a number")
+number = input()
 
 # Convert number into 6 digit format
+six_digit_num = str(number).zfill(6)
 
 # Print result
+print(six_digit_num)
