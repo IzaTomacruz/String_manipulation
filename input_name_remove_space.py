6ed09c7f94824099d7e2629470905ed57c9ecf3e
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-22 07:14:07 +0800

    Removed the instruction

--- input_name_remove_space.py
+++ input_name_remove_space.py
@@ -1,8 +1,3 @@
-#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
-#Example:
-#Input:         Juan Dela Cruz
-#Output: Juan Dela Cruz
-
 # Ask for a full name
 print("What is your name?")
 name = input("Name: ")
