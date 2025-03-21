fc9ab7541585bbc35884292b7fa4284ce51141c3
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-22 07:01:46 +0800

    Created program for input_name_remove_space.py

--- input_name_remove_space.py
+++ input_name_remove_space.py
@@ -4,7 +4,11 @@
 #Output: Juan Dela Cruz
 
 # Ask for a full name
+print("What is your name?")
+name = input("Name: ")
 
 # Remove the spaces from the beginning of the full name 
+removed_space = name.lstrip()
 
 # Print result
+print("\nOutput:", removed_space)
