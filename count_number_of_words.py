6783ff6dd2e346c401966c603c5ac6b6bf11e217
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-22 08:04:08 +0800

    Created a program for count_number_of_words.py

--- count_number_of_words.py
+++ count_number_of_words.py
@@ -1,10 +1,9 @@
-#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
-#Example:
-#Input: We will weather the weather whatever the weather whether we like it or not
-#Output: 14
-
 # Ask for a complete statement
+print("Input your statement")
+statement = input("\nStatement: ")
 
 # Count the number of words in the statement
+count_words = len(statement.split())
 
 # Print the result
+print("Number of words:", count_words)
